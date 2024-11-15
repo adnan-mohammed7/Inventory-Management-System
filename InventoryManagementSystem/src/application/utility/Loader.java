afb36ece6e7e33b5b288ebd3f157848175144950
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-11-15 18:41:56 -0500

    Student details  added project ready for submission

--- InventoryManagementSystem/src/application/utility/Loader.java
+++ InventoryManagementSystem/src/application/utility/Loader.java
@@ -1,3 +1,15 @@
+/**********************************************
+Workshop #4&5
+Course:<APD 545> - Fall 2024
+Last Name: Mohammed
+First Name: Adnan
+ID: 174731216
+Section: ZAA
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature
+Date: 15th November 2024
+**********************************************/
+
 package application.utility;
 
 import application.controllers.MainController;
