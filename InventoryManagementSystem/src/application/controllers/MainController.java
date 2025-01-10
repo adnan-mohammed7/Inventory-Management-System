19d306bc67c8debde50d94c2b193424ef633e07d
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2025-01-10 12:36:14 -0500

    Removed all the assignment credentials

--- InventoryManagementSystem/src/application/controllers/MainController.java
+++ InventoryManagementSystem/src/application/controllers/MainController.java
@@ -1,15 +1,3 @@
-/**********************************************
-Workshop #4&5
-Course:<APD 545> - Fall 2024
-Last Name: Mohammed
-First Name: Adnan
-ID: 174731216
-Section: ZAA
-This assignment represents my own work in accordance with Seneca Academic Policy.
-Signature
-Date: 15th November 2024
-**********************************************/
-
 package application.controllers;
 
 import java.io.FileInputStream;
